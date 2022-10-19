753b11a54bbf9ed8fc4aa72d8dd4438dfa193869
Victor Malhotra <victorrmalhotra@gmail.com>
2022-10-19 15:01:46 -0700

    Make a few functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
-const url="https://course-api.com/react-tours-project"
+import React,{useState,useEffect} from 'react';
+import ReactReadMoreReadLess from 'react-read-more-read-less';
+const url="https://course-api.com/react-tours-project";
 
 function App() {
-   const [data,setData]=useState([]);
+  const [data,setData]=useState([]);
+          const [readMore, setReadMore] = useState(true);
 
   const getData=async () => {
     const response=await fetch(url);
@@ -21,17 +23,35 @@ function App() {
         {data.map((place) => {
           const {id,name,info,image,price}=place;
           return (
-            <li key={id}>
-              <img src={image} alt={name} />
-              <div>
+            <div  key={id} className="max-w-sm rounded overflow-hidden shadow-lg">
+            <li>
+              <img  className="w-full" src={image} alt={name} />
+                <div className='px-6 py-4'>
+                  <div>
                 <h4>{name}</h4>
-                <h5>{price}</h5>
+                    <h5>{price}</h5>
+                    </div>
+                 <ReactReadMoreReadLess
+        charLimit={200}
+        readMoreText={"Read more ▼"}
+        readLessText={"Read less ▲"}
+        readMoreClassName="read-more-less--more"
+        readLessClassName="read-more-less--less"
+      >
+        {info}
+      </ReactReadMoreReadLess>
               </div>
-              <article className='line-clamp-3'>
-                {info}
-                
-              </article>
+              <button className="border-2 border-rose-600 text-rose-600" onClick={() => {
+          const getData=async () => {
+          const response=await fetch(url);
+          let data=await response.json()
+          delete data[id]
+          setData(data);
+        };
+        getData()
+      }}>Not Intrested</button>
             </li>
+              </div>
           );
         })}
       </ul>
